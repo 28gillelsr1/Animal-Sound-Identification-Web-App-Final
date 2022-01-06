b59c56966c7574f172377fd268cb46cf30d1c845
Srivar Gillela <79170238+28gillelsr1@users.noreply.github.com>
2022-01-06 16:52:14 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,54 @@
+function startClassification()
+{
+  navigator.mediaDevices.getUserMedia({ audio: true, video:false});
+  classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/745z9mb8O/model.json', { probabilityThreshold: 0.7 } ,modelReady);
+}
+
+function modelReady(){
+  classifier.classify(gotResults);
+}
+
+var dog = 0;
+var cat = 0;
+var lion=0;
+var cow=0;
+
+function gotResults(error, results) {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(results);
+    random_number_r = Math.floor(Math.random() * 255) + 1;
+    random_number_g = Math.floor(Math.random() * 255) + 1;
+    random_number_b = Math.floor(Math.random() * 255) + 1;
+
+
+    document.getElementById("result_label").innerHTML = 'Detected voice is of  - '+ results[0].label;
+    document.getElementById("result_confidence").innerHTML = 'Detected Dog - '+dog+'   Detected Cat - '+cat+"<br>"+  '    Detected Lion - '+lion+ '   Detected Cow - '+cow;
+    document.getElementById("result_label").style.color = "rgb("+random_number_r+","+random_number_g+","+random_number_r+")";
+    document.getElementById("result_confidence").style.color = "rgb("+random_number_r+","+random_number_g+","+random_number_r+")";
+
+    img = document.getElementById('animal_image');
+
+    if (results[0].label == "Barking") {
+      img.src = 'bark.gif';
+      dog = dog+1;
+    } else if (results[0].label == "Meowing") {
+      img.src = 'meow.gif';
+      cat = cat + 1;
+    } 
+    else if (results[0].label == "Roaring") {
+        img.src = 'lion-roaring.gif';
+        lion = lion+1;
+    }
+        else if (results[0].label == "Mooing") {
+            img.src = 'cow_mooing.gif';
+            cow = cow+1;
+        }
+        else if (results[0].label == "Background Noise") {
+            img.src = 'https://shravaripatil.github.io/Sound_controlled_animals/listen.gif';
+        
+        } 
+  }
+}
+  
